b5033d78fddcb35530ac1a70dc44f3558bea9a39
jake <jake@vertaxe.com>
2017-04-26 19:58:10 +0200

    basics layout

--- app/modules/Projects/Show/sections/Basics.js
+++ app/modules/Projects/Show/sections/Basics.js
@@ -6,11 +6,14 @@ export default class Basics extends React.PureComponent { // eslint-disable-line
     return (
       <div className="row">
         <div className="col-xs-12 col-md-7">
-          <img src="" role="presentation" />
+          <img src={p.images[0].url} role="presentation" />
         </div>
         <div className="col-xs-12 col-md-5">
           <form>
-            <input type="text" placeholder="Name" defaultValue={p.name} />
+            <div className="flex justify-between">
+              <label>Name</label>
+              <input className="x-max20" type="text" placeholder="Name" defaultValue={p.name} />
+            </div>
           </form>
         </div>
       </div>
