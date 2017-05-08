1389c2fc99680500836bbee2d0c4775b9e7c5e8a
jake <jake@vertaxe.com>
2017-05-09 00:18:14 +0200

    replace react-widgets with custom dropdown

--- app/components/SelectInput/index.js
+++ app/components/SelectInput/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import DropdownList from 'react-widgets/lib/DropdownList';
 
-export default function SelectInput({ input, data, meta }) {
-  let val = input.value;
-  if (val.toJS) { val = val.toJS(); }
-  return (
-    <DropdownList
-      data={data}
-      name={input.name}
-      defaultValue={val}
-      valueField="id"
-      textField="name"
-      onSelect={input.onChange}
-    />
-  );
+export default class SelectInput extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  componentWillMount() {
+    this.setState({ active: false });
+  }
+  toggleMenu() {
+    {
+      this.state.active
+      ? this.setState({ active: false })
+      : this.setState({ active: true })
+    }
+  }
+  handleClick(item) {
+    const { input } = this.props;
+    input.onChange(item);
+    this.setState({ active: false });
+  }
+  render() {
+
+    const { input, data, meta } = this.props;
+    let val = input.value;
+    if (val.toJS) { val = val.toJS(); }
+
+    const options = data.map((item, index) => (
+      <li key={`select-${input.name}-${index}`} onClick={() => { this.handleClick(item); }}>
+        {item.name}
+      </li>
+    ));
+
+    return (
+      <div className="select-input">
+        <button onClick={() => { this.toggleMenu(); }}>{val.name}</button>
+        {this.state.active &&
+          <ul>
+            {options}
+          </ul>
+        }
+      </div>
+    );
+  }
 }
