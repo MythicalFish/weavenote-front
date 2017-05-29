53e77c6398de6f670ab22af61d16e3b591e00092
jake <jake@vertaxe.com>
2017-05-29 12:56:06 +0200

    add image

--- app/containers/ProjectManager/partials/Instructions/Form.js
+++ app/containers/ProjectManager/partials/Instructions/Form.js
@@ -5,12 +5,16 @@ import DataRow from 'components/DataRow';
 const Form = (props) => {
 
   const { handleSubmit, submitting, initialValues } = props;
+  const img = initialValues.get('image');
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="data-rows">
         <Field name="title" type="text" component={DataRow} label="Title" focus />
         <Field name="description" type="textarea" component={DataRow} label="Description" />
+        {img && 
+          <img src={img.get('url')} />  
+        }
         <footer className="p2 center">
           <button className="btn-color2x" type="submit" disabled={submitting}>Save</button>
         </footer>
