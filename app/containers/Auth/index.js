cc287bfe90bbabdb613b5e7066cf4a036b9b9e3a
jake <jake@vertaxe.com>
2017-06-01 18:17:35 +0200

    attempt tp externalize auth0

--- app/containers/Auth/index.js
+++ app/containers/Auth/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
-import * as authUtils from 'utils/authUtils';
-import AuthService from './AuthService';
+import { loggedIn } from 'utils/authUtils';
 
 export default class Auth extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  componentDidMount() {
-    if (!authUtils.loggedIn()) {
-      const Auth0 = new AuthService();
-      Auth0.lock.show();
-    }
+
+  LOCK_OPTIONS = {
+    container: 'login-form',
+    theme: {},
+    auth: {
+      redirectUrl: window.location.origin,
+      responseType: 'token',
+      params: {
+        scope: 'openid email',
+      },
+    },
   }
 
+  componentDidMount() {
+    var addScript = document.createElement('script');
+    addScript.setAttribute('src', 'https://cdn.auth0.com/js/lock/10.13.0/lock.min.js');
+    document.body.appendChild(addScript);
+    setTimeout(() => {
+      var lock = new Auth0Lock(
+        process.env.AUTH0_CLIENT_ID,
+        process.env.AUTH0_DOMAIN,
+        this.LOCK_OPTIONS
+      );
+console.log(lock)
+      lock.on("authenticated", function (authResult) {
+        lock.getUserInfo(authResult.accessToken, function (error, profile) {
+          if (error) {
+            console.error('Authentication Error', error);
+            return;
+          }
+
+          localStorage.setItem('access_token', authResult.accessToken);
+          localStorage.setItem('id_token', authResult.idToken);
+          localStorage.setItem('profile', JSON.stringify(profile));
+        });
+      });
+    }, 1000);
+  }
+
+  createLock = () => (
+    // Included externally and initialized with inline ES5 to avoid having to bundle auth0-lock as well
+    <script>
+      {`
+        var lock = new Auth0Lock(
+          "${process.env.AUTH0_CLIENT_ID}",
+          "${process.env.AUTH0_DOMAIN}",
+          {
+            container: 'login-form',
+            theme: {},
+            auth: {
+              redirectUrl: window.location.origin,
+              responseType: 'token',
+              params: {
+                scope: 'openid email',
+              },
+            },
+          }
+        );
+
+        lock.on("authenticated", function(authResult) {
+          lock.getUserInfo(authResult.accessToken, function (error, profile) {
+            if (error) {
+              console.error('Authentication Error', error);
+              return;
+            }
+
+            localStorage.setItem('access_token', authResult.accessToken);
+            localStorage.setItem('id_token', authResult.idToken);
+            localStorage.setItem('profile', JSON.stringify(profile));
+          });
+        });
+      `}
+    </script>
+  )
+
   render() {
-    if (authUtils.loggedIn()) return this.props.children;
+    if (loggedIn()) return this.props.children;
     return (
       <div className="flex items-middle vh-ymin100 bg-gray-darker">
         <div>
@@ -19,18 +86,6 @@ export default class Auth extends React.PureComponent { // eslint-disable-line r
             Seamless
           </div>
           <div id="login-form"></div>
-          <style>{`
-            .auth0-lock-header {
-              display: none;
-            }
-            .auth0-lock.auth0-lock.auth0-lock-opened-in-frame {
-              border-radius: 6px;
-              overflow: hidden;
-            }
-            .auth0-label-submit {
-              font-weight: bold;
-            }
-          `}</style>
         </div>
       </div>
     );
