7f719d4caa99a5480109ec2efcd504a493b0db90
jake <jake@vertaxe.com>
2017-06-08 12:21:10 +0200

    error styling

--- app/utils/API.js
+++ app/utils/API.js
@@ -68,12 +68,6 @@ const encodedRequestParams = (params) => {
     .join('&');
 };
 
-function asdasd(response) {
-  console.log('asdasd');
-  console.log(response);
-  return response;
-}
-
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
@@ -84,14 +78,9 @@ function checkStatus(response) {
 }
 
 const ConsoleErrorStyle = [
-  'background: linear-gradient(#D33106, #571402)',
-  'border: 1px solid #3E0E02',
-  'color: white',
+  'background: #111',
+  'color: #51B2FE',
   'display: block',
-  'padding: 20px',
-  'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)',
-  'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
-  'line-height: 44px',
-  'text-align: center',
-  'font-weight: bold',
+  'padding: 10px',
+  'line-height: 30px',
 ].join(';');
