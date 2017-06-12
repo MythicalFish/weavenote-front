d6c44a434e9104a5ea3a30f6a8883deb8e60ec31
jake <jake@vertaxe.com>
2017-06-12 15:55:04 +0200

    separate Input from DataRow

--- app/components/DataRow.js
+++ app/components/DataRow.js
@@ -1,87 +1,29 @@
-import React, { PropTypes } from 'react';
-import SelectInput from 'components/SelectInput';
+import React from 'react';
+import Input from 'components/Input';
 
-export default class DataRow extends React.PureComponent {
-
-  componentDidMount() {
-    if (this.props.focus) {
-      this.nameInput.focus();
-    }
-  }
-
+export default class DataRow extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
-
     const { props } = this;
     const { label, type, className } = props;
-    let field = null;
-    let touched = false;
-    let error = false;
+    const fieldProps = props;
     let rowClass = '';
-    let fieldProps;
+    let fieldClass = '';
 
-    if (props.input) {
-      fieldProps = props.input;
-      if (props.onChanged) {
-        fieldProps.onChanged = props.onChanged; // onChange in use by redux-form
-      }
-      if (props.tail) {
-        fieldProps.tail = props.tail;
-      }
+    if (type === 'textarea') {
+      rowClass = 'flex-wrap';
+      fieldClass = 'x-fill';
     } else {
-      fieldProps = props;
-    }
-
-    if (props.focus) {
-      fieldProps.ref = (input) => { this.nameInput = input; };
-    }
-
-    if (props.noEdit) {
-      fieldProps.readOnly = true;
-    }
-
-    if (props.meta) {
-      touched = props.meta.touched;
-      error = props.meta.error;
-    }
-
-    if (type === 'select') {
-      if (!fieldProps.data && props.data) {
-        fieldProps.data = props.data;
-      }
-    }
-
-    switch (type) {
-      case 'display':
-        field = <div className="right-align">{props.value}</div>;
-        break;
-      case 'select':
-        field = <SelectInput {...fieldProps} className="right-align" />;
-        break;
-      case 'textarea':
-        field = <textarea {...fieldProps} />;
-        rowClass = 'flex-wrap';
-        break;
-      default:
-        field = <input {...fieldProps} type={type} className="right-align" />;
-        break;
+      fieldClass = 'right-align';
     }
 
     return (
       <div className={`data-row ${rowClass}${className || ''}`}>
         <label>{label}</label>
-        {field}
-        <div className="focused p0"></div>
-        {touched && error && <span className="error">{error}</span>}
+        <Input {...fieldProps} fieldClass={fieldClass} />
       </div>
     );
   }
 }
 
 DataRow.propTypes = {
-  focus: PropTypes.bool,
-  value: PropTypes.any,
-  label: PropTypes.string,
-  type: PropTypes.string,
-  input: PropTypes.object,
-  meta: PropTypes.object,
 };
---
+++ app/components/Input.js
@@ -0,0 +1,84 @@
+import React, { PropTypes } from 'react';
+import SelectInput from 'components/SelectInput';
+
+export default class Input extends React.PureComponent {
+
+  componentDidMount() {
+    if (this.props.focus) {
+      this.nameInput.focus();
+    }
+  }
+
+  render() {
+
+    const { props } = this;
+    const { fieldClass, type } = props;
+    let field = null;
+    let touched = false;
+    let error = false;
+    let fieldProps;
+
+    if (props.input) {
+      fieldProps = props.input;
+      if (props.onChanged) {
+        fieldProps.onChanged = props.onChanged; // onChange in use by redux-form
+      }
+      if (props.tail) {
+        fieldProps.tail = props.tail;
+      }
+    } else {
+      fieldProps = props;
+    }
+
+    if (props.focus) {
+      fieldProps.ref = (input) => { this.nameInput = input; };
+    }
+
+    if (props.noEdit) {
+      fieldProps.readOnly = true;
+    }
+
+    if (props.meta) {
+      touched = props.meta.touched;
+      error = props.meta.error;
+    }
+
+    if (type === 'select') {
+      if (!fieldProps.data && props.data) {
+        fieldProps.data = props.data;
+      }
+    }
+
+    switch (type) {
+      case 'display':
+        field = <div className="readonly">{props.value}</div>;
+        break;
+      case 'select':
+        field = <SelectInput {...fieldProps} />;
+        break;
+      case 'textarea':
+        field = <textarea {...fieldProps} />;
+        break;
+      default:
+        field = <input {...fieldProps} type={type} />;
+        break;
+    }
+
+    return (
+      <div className={`input ${fieldClass}`}>
+        {field}
+        <div className="focused p0"></div>
+        {touched && error && <span className="error">{error}</span>}
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  focus: PropTypes.bool,
+  value: PropTypes.any,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  input: PropTypes.object,
+  meta: PropTypes.object,
+};
