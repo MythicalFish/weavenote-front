420a6c8e80dd2f3a49a244e42f364a01516923f8
jake <jake@vertaxe.com>
2017-06-28 19:30:14 +0200

    trim the fat from AuthService

--- app/utils/AuthService.js
+++ app/utils/AuthService.js
@@ -1,39 +1,18 @@
-import { EventEmitter } from 'events';
 import Auth0Lock from 'auth0-lock';
 
-export default class AuthService extends EventEmitter {
+export default class AuthService {
 
   constructor(options) {
-    super();
     this.lock = new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, options);
     this.lock.on('authenticated', this.doAuthentication.bind(this));
     this.lock.on('authorization_error', this.err.bind(this));
   }
 
   doAuthentication(authResult) {
-    this.set('id_token', authResult.idToken);
-    this.set('access_token', authResult.accessToken);
-    this.lock.getProfile(authResult.idToken, (error, profile) => {
-      if (error) {
-        console.error('Error loading the Profile', error); // eslint-disable-line no-console
-      } else {
-        this.setProfile(profile);
-      }
-    });
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('access_token', authResult.accessToken);
   }
 
   err = (error) => console.error('Authentication Error', error); // eslint-disable-line no-console
-  get = (item) => localStorage.getItem(item);
-  set = (item, value) => localStorage.setItem(item, value);
-
-  setProfile(profile) {
-    localStorage.setItem('profile', JSON.stringify(profile));
-    this.emit('profile_updated', profile);
-  }
-
-  getProfile() {
-    const profile = localStorage.getItem('profile');
-    return profile ? JSON.parse(localStorage.profile) : {};
-  }
 
 }
