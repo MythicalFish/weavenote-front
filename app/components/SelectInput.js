3b0ecc4f877bdae68b064227bb6f8f883db070c1
jake <jake@vertaxe.com>
2017-07-05 19:49:03 +0200

    refactor SelectInput

--- app/components/SelectInput.js
+++ app/components/SelectInput.js
@@ -1,82 +1,108 @@
 import React, { PropTypes } from 'react';
 import Dot from 'components/Dot';
 import PriceSymbol from 'components/PriceSymbol';
-import TetherComponent from 'react-tether'
+import TetherComponent from 'react-tether';
 
-export default class SelectInput extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
-  state = { active: false }
+export default class SelectInput extends React.PureComponent {
+  state = { active: false };
 
   toggleState = () => {
     if (!this.props.readOnly) {
       this.setState({ active: !this.state.active });
     }
-  }
+  };
 
   handleClick = (item) => {
     this.setState({ active: false });
     const { onChange, onChanged } = this.props;
     if (onChange) onChange(item);
     if (onChanged) onChanged(item);
-  }
+  };
 
-  render() {
-    const { value, data, className, tail, align, readOnly } = this.props;
-    let inputClass = className;
-    let val = value || {};
-    let alignment = align || 'left';
+  label = () => {
+    const { value, button } = this.props;
+    if (button) return button;
+    let val = value;
+    if (!val) val = { name: null };
+    if (val.toJS) val = value.toJS();
+    return (
+      <button onClick={this.toggleState} type="button">
+        {val.name}
+        {val.iso_code &&
+          <PriceSymbol code={val.iso_code} className="bold ml1" />}
+        {val.hex_code && <Dot className="ml1" color={val.hex_code} />}
+      </button>
+    );
+  };
+
+  items = () => {
+    const { value, data, children, align } = this.props;
+    const alignment = align || 'left';
     const alignClass = `${alignment}-align`;
-    switch (alignment) {
-      case 'left':
-        alignment = 'right';
-        break;
-      case 'right':
-        alignment = 'left';
-        break;
-      default:
-        break;  
-    }
-    const attachment = `top ${alignment}`;
-    if (readOnly) { inputClass += ' noselect'; }
-    if (val && val.toJS) { val = val.toJS(); }
-    const options = [];
+    let items;
     if (data) {
-      data.forEach((item, index) => {
+      items = [];
+      data.forEach((item) => {
         if (item === value) return;
         let i = item;
         if (i.toJS) i = i.toJS();
-        options.push((
-          <li key={`select-${name}-${index}`} onClick={() => { this.handleClick(item); }}>
+        items.push(
+          <li
+            key={item}
+            onClick={() => {
+              this.handleClick(item);
+            }}
+          >
             {i.name || i.label}
-            {val.iso_code && <PriceSymbol code={i.iso_code} className="bold ml1" />}
+            {value.iso_code &&
+              <PriceSymbol code={i.iso_code} className="bold ml1" />}
             {i.hex_code && <Dot className="ml1" color={i.hex_code} />}
           </li>
-        ));
+        );
       });
     }
+    return (
+      <ul className={`select-input-options ${alignClass}`}>
+        {items || children}
+      </ul>
+    );
+  };
 
+  tail = () => {
+    const { tail } = this.props;
+    if (tail) {
+      return tail({
+        onClick: () => {
+          this.setState({ active: false });
+        },
+      });
+    }
+    return null;
+  };
+
+  tetherOptions = {
+    attachment: 'top right',
+    targetAttachment: 'bottom right',
+    constraints: [
+      {
+        to: 'scrollParent',
+      },
+    ],
+  };
+
+  render() {
+    const { className, readOnly } = this.props;
+    let inputClass = className;
+    if (readOnly) inputClass += ' noselect';
     return (
       <div className={`select-input p0 ${inputClass}`}>
-        <TetherComponent
-          attachment={attachment}
-          constraints={[{
-            to: 'scrollParent',
-            //attachment: 'together',
-          }]}
-        >
-          <button onClick={this.toggleState} type="button">
-            {val.name}
-            {val.iso_code && <PriceSymbol code={val.iso_code} className="bold ml1" />}
-            {val.hex_code && <Dot className="ml1" color={val.hex_code} />}
-          </button>
+        <TetherComponent {...this.tetherOptions}>
+          {this.label()}
           {this.state.active &&
-            <ul className={`select-input-options ${alignClass}`}>
-              {options}
-              {tail &&
-                tail({ onClick: () => { this.setState({ active: false }); } })
-              }
-            </ul>
-          }
+            <div>
+              {this.items()}
+              {this.tail()}
+            </div>}
         </TetherComponent>
       </div>
     );
@@ -84,18 +110,14 @@ export default class SelectInput extends React.Component { // eslint-disable-lin
 }
 
 SelectInput.propTypes = {
-  value: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.string,
-  ]),
-  data: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object,
-  ]),
+  value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  data: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
   className: PropTypes.string,
   onChange: PropTypes.func,
   onChanged: PropTypes.func,
   tail: PropTypes.func,
   readOnly: PropTypes.bool,
   align: PropTypes.string,
+  children: PropTypes.node,
+  button: PropTypes.node,
 };
