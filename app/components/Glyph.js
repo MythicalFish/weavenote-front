41746cae348737f418ab884ec9380257ef5ea313
jake <jake@vertaxe.com>
2017-07-12 16:18:34 +0200

    refactor glyph

---
+++ app/components/Glyph.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+
+const Glyph = (props) => {
+  const Icon = () => {
+    const color = props.color || 'gray';
+    return <i className={`fa fa-${props.icon} ${color}`} />;
+  };
+  const bProps = {
+    className: 'glyph',
+  };
+  if (props.onClick) {
+    return (
+      <button {...bProps} onClick={props.onClick}>
+        <Icon />
+      </button>
+    );
+  } else if (props.to) {
+    return (
+      <Link {...bProps} to={props.to}>
+        <Icon />
+      </Link>
+    );
+  }
+  return (
+    <div {...bProps}>
+      <Icon />
+      {props.children && props.children}
+    </div>
+  );
+};
+
+Glyph.propTypes = {
+  onClick: PropTypes.func,
+  to: PropTypes.string,
+  color: PropTypes.string,
+  icon: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Glyph;
--- app/components/PlusButton.js
+++ app/components/PlusButton.js
@@ -1,39 +1,8 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
+import Glyph from 'components/Glyph';
 
 export default function PlusButton(props) {
-  const bProps = {
-    className: 'glyph',
-  };
-  const iProps = {
-    className: 'fa fa-plus-circle',
-  };
-  if (props.color) {
-    iProps.className += ` ${props.color}`;
-  } else {
-    iProps.className += ' gray';
-  }
-  const icon = <i {...iProps} />;
-  if (props.onClick) {
-    return (
-      <button {...bProps} onClick={props.onClick}>
-        {icon}
-      </button>
-    );
-  } else if (props.to) {
-    return (
-      <Link {...bProps} to={props.to}>
-        {icon}
-      </Link>
-    );
-  } else if (props.children) {
-    return (
-      <div {...bProps}>
-        {icon}
-        {props.children}
-      </div>
-    );
-  }
+  return <Glyph icon="plus-circle" {...props} />;
 }
 
 PlusButton.propTypes = {
