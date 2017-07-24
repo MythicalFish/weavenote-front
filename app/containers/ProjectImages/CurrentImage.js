489641e0810ad000a7eed4368bb69221aa66c4ba
jake <jake@vertaxe.com>
2017-07-24 20:55:43 +0200

    add canvas

--- app/containers/ProjectImages/CurrentImage.js
+++ app/containers/ProjectImages/CurrentImage.js
@@ -1,22 +1,33 @@
 import React, { PropTypes } from 'react';
 import Image from 'components/Image';
+import { Layer, Rect, Stage, Group } from 'react-konva';
 
-class CurrentImage extends React.PureComponent {
+class CurrentImage extends React.Component {
   render() {
     const { currentImage, annotation, setAnnotation } = this.props;
     const iProps = {
       src: currentImage.getIn(['urls', 'medium']),
     };
+    let Dot = <Rect x={10} y={10} width={10} height={10} fill="blue" />;
     if (annotation) {
       iProps.className = annotation ? 'cursor-crosshair' : '';
       iProps.onClick = (c) => {
         const e = c.nativeEvent;
         setAnnotation({ x: e.offsetX, y: e.offsetY });
       };
+      if (annotation.position) {
+        const pos = annotation.position;
+        Dot = <Rect x={pos.x} y={pos.y} width={10} height={10} fill="blue" />;
+      }
     }
     return (
-      <div>
+      <div className="canvas-container">
         <Image {...iProps} />
+        <div className="canvas">
+          <Stage>
+            <Layer />
+          </Stage>
+        </div>
       </div>
     );
   }
